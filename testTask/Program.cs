c1164e74e40d37b6cd3e71f82d682a4554a103de
Ivan Vlasiuk <vlasiukk.i.v@gmail.com>
2024-04-16 16:41:25 +0300

    updated favicon

--- testTask/Program.cs
+++ testTask/Program.cs
@@ -1,24 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using testTask.Context;
+using testTask.Repositories.Implementations;
+using testTask.Repositories.Interfaces;
+using testTask.Services.Implementations;
+using testTask.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IFilmRepository, FilmRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IFilmCategoryRepository, FilmCategoryRepository>();
+
+builder.Services.AddScoped<IFilmService, FilmService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IFilmCategoryService, FilmCategoryService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
-
-app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
