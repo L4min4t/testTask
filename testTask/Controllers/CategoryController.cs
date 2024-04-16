fd48e2212c66938bfd0d7a6b3b4f3a0db8da18e2
Ivan Vlasiuk <vlasiukk.i.v@gmail.com>
2024-04-16 16:37:57 +0300

    added controllers

---
+++ testTask/Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace testTask.Controllers;
+
+public class CategoryController(ILogger<CategoryController> logger) : Controller
+{
+    private readonly ILogger<CategoryController> _logger = logger;
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ testTask/Controllers/FilmController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace testTask.Controllers;
+
+public class FilmController(ILogger<FilmController> logger) : Controller
+{
+    private readonly ILogger<FilmController> _logger = logger;
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+}
